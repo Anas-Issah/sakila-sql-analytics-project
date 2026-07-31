76c8b9bb43aadd5d7e3268910d98dd2212a2a3c6
Anas Issah <anasisa01@gmail.com>
2026-07-31 12:52:09 +0000

    Add first basic SQL queries

--- queries/01_basic_queries/query_001_customer_list.sql
+++ queries/01_basic_queries/query_001_customer_list.sql
@@ -1,1 +1,5 @@
 
+--The marketing department wants a list of all customers in the database.
+SELECT customer_id,first_name,last_name,email FROM customer
+ORDER BY customer_id ASC;
+
---
+++ queries/01_basic_queries/query_005_available_movie_ratings_list.sql
@@ -0,0 +1,2 @@
+--Management wants to know all the unique movie rating categories available.
+SELECT DISTINCT rating FROM film;
